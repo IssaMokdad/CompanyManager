6e9333104512f3bc3ae697aed9a78802cc441658
Husseind1 <hussein.m.darwich@gmail.com>
2020-04-19 14:42:12 +0300

    Hello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
+                    <h1>test</h1>
                 </div>
 
                 <div class="links">
